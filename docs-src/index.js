406bb656c3cc850b47e369232159dd0af3135e1b
Damir <laraloop.com@gmail.com>
2022-11-04 08:56:46 +0100

    Update docs

--- docs-src/index.js
+++ docs-src/index.js
@@ -16,10 +16,6 @@ const md = './docs-src/md';
 const options = {};
 
 readdirSync(src).forEach(file => {
-  if (file !== 'test.html') {
-    return;
-  }
-
   const html = readFileSync(path.resolve(`${src}${file}`), 'utf-8');
 
   posthtml([
